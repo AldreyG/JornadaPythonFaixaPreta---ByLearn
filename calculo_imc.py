d4348d7f6b74af5bf3b8330b0fd05ef62c9b7994
Aldrey Georgetti <62533150+AldreyG@users.noreply.github.com>
2021-05-06 23:11:02 -0300

    Create calculo_imc.py

---
+++ calculo_imc.py
@@ -0,0 +1,29 @@
+def numero_quadrado(numero):
+  quadrado = numero * numero 
+  return quadrado
+
+def calcular_imc(peso, altura):
+  altura_quadrada = numero_quadrado(altura)
+  imc = peso / altura_quadrada
+  
+  return imc
+
+def classificar(imc):
+  if imc < 18.5:
+    print('Muito magro')
+  elif imc >= 18.5 and imc < 25:
+    print('Normal')
+  elif imc >= 25 and imc < 30:
+    print('Sobrepeso')
+  elif imc >= 30 and imc < 35:
+    print('Obesidade grau I')
+  elif imc >= 35 and imc < 40:
+    print('Obesidade grau II (Obesidade Severa)')
+  else:
+    print('Obesidade grau III(Obesidade Mórbida)')
+
+peso = float(input('Insira seu peso(Kg): '))
+altura = float(input('Insira sua altura(M): '))
+
+meu_imc = calcular_imc(peso, altura)
+classificar(meu_imc)
